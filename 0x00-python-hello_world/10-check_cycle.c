cfc8025117b3dccd79462627cc4b4164bec3727c
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-10 04:20:47 +0000

    created script for exercise 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a list is a cycle or not
+ * @list: struct
+ * Return: 1 if is a cyrcle or 0
+ */
+int check_cycle(listint_t *list)
+{
+	if (list != NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
