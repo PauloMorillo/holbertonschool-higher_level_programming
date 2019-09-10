9a9f44642fd3b086801d70082640f3021d363779
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-10 18:24:11 +0000

    fixed remove end space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,15 +3,14 @@ def fizzbuzz():
     count = 1
     while count <= 100:
         if ((count % 3) == 0) and ((count % 5) == 0):
-            print("{}".format("FizzBuzz "), end="")
+            print("{} ".format("FizzBuzz"), end="")
             count = count + 1
         if (count % 3) == 0:
-            print("{}".format("Fizz "), end="")
+            print("{} ".format("Fizz"), end="")
 #            count = count + 1
         elif (count % 5) == 0:
-            print("{}".format("Buzz "), end="")
+            print("{} ".format("Buzz"), end="")
 #            count = count + 1
         else:
             print("{:d} ".format(count), end="")
         count = count + 1
-    print("")
