7d857bae485a04844467902ba1aeff3d8e637405
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-10 17:07:37 +0000

    fixed remove spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ elif number < 0:
 else:
     lastn = 0
 if lastn > 5:
-    print("Last digit of ", number, " is ", lastn, " and is greater than 5")
+    print("Last digit of", number, "is", lastn, "and is greater than 5")
 elif lastn < 6 and lastn != 0:
-    print("Last digit of ", number, " is ", lastn,
-          " and is less than 6 and not 0")
+    print("Last digit of", number, "is", lastn,
+          "and is less than 6 and not 0")
 if lastn == 0:
-    print("Last digit of ", number, " is ", lastn, " and is 0")
+    print("Last digit of", number, "is", lastn, "and is 0")
