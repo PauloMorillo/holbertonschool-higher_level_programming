5d8ba6833a1501bb066520d35f7d981c1a14b5de
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-10 18:22:08 +0000

    created script for exercise 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    count = 1
+    while count <= 100:
+        if ((count % 3) == 0) and ((count % 5) == 0):
+            print("{}".format("FizzBuzz "), end="")
+            count = count + 1
+        if (count % 3) == 0:
+            print("{}".format("Fizz "), end="")
+#            count = count + 1
+        elif (count % 5) == 0:
+            print("{}".format("Buzz "), end="")
+#            count = count + 1
+        else:
+            print("{:d} ".format(count), end="")
+        count = count + 1
+    print("")
