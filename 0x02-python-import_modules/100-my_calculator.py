50227aeea51267d4618f87b00b93c6b24799b68d
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-12 22:18:05 +0000

    created script for exercise 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+argv = sys.argv
+argc = len(argv)
+if __name__ == "__main__":
+    if argc == 4:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == '+':
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            exit(0)
+        elif argv[2] == '-':
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            exit(0)
+        elif argv[2] == '*':
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            exit(0)
+        elif argv[2] == '/':
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
