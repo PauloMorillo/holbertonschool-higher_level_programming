6a1e0f42f4802d4ae9891f4c28eebcdbe8853fc9
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-12 17:08:31 +0000

    fixed with all the function after name declare

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import add_0
+"""import add_0 as ad
 a = 1
 b = 2
+"""
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    import add_0 as ad
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, ad.add(a, b)))
