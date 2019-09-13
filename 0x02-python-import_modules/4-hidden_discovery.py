edd4488716aa31756d454872821af251258696d2
root <pauloan@hotmail.com>
2019-09-13 01:36:50 +0000

    fixed with pep

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import hidden_4
-   dire = dir(hidden_4)
-   a = 0
-   while a < len(dire):
-      if dire[a][0] != '_':
-         print(dire[a])
-      a = a + 1
+    import hidden_4
+    dire = dir(hidden_4)
+    a = 0
+    while a < len(dire):
+        if dire[a][0] != '_' and dire[a][0] != '_':
+            print(dire[a])
+        a = a + 1
