eb05164bd10ad1f942e93fbe11b89a215cdfd4d2
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-17 23:00:06 +0000

    created script for exercise 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        a = 0
+        t = []
+        while a < len(my_list):
+            if a % 2 == 0:
+                t.append(True)
+            else:
+                t.append(False)
+            a = a + 1
+        return t
