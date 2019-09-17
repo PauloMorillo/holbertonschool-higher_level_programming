fc1703c61ea33c885992afba551b199a5c938c5a
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-17 23:13:02 +0000

    fixed with condition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,13 @@
 def divisible_by_2(my_list=[]):
         a = 0
         t = []
-        while a < len(my_list):
-            if a % 2 == 0:
-                t.append(True)
-            else:
-                t.append(False)
-            a = a + 1
-        return t
+        if my_list:
+                while a < len(my_list):
+                        if a % 2 == 0:
+                                t.append(True)
+                        else:
+                                t.append(False)
+                        a = a + 1
+                return t
+        else:
+                return False
