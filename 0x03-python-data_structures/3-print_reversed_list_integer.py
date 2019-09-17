6f563867431f129fa5de8bea9bd2e8ed51aae875
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-17 18:02:26 +0000

    fixed with condition exist list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     a = 0
-    my_list.reverse()
-    while a < len(my_list):
-        print("{:d}".format(my_list[a]))
-        a = a + 1
-    my_list.reverse()
+    if my_list:
+        my_list.reverse()
+        while a < len(my_list):
+            print("{:d}".format(my_list[a]))
+            a = a + 1
+        my_list.reverse()
