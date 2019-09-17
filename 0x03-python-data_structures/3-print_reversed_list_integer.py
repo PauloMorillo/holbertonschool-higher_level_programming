e6181c2f749276c86b295a460143dad3f80b024d
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-17 17:55:50 +0000

    change with reverse function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    a = -1
-    while a >= len(my_list) * -1:
+    a = 0
+    my_list.reverse()
+    while a < len(my_list):
         print("{:d}".format(my_list[a]))
-        a = a - 1
+        a = a + 1
+    my_list.reverse()
