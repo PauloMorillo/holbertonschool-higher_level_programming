cae3429709d4286d89674eb895e453ae6c315f5b
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-17 17:49:13 +0000

    fixed with 0 in conditio

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cmy_list = my_list[0:]
-    if idx < len(cmy_list) and idx > 0:
+    if idx < len(cmy_list) and idx >= 0:
         cmy_list[idx] = element
         return cmy_list
     else:
