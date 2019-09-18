98c460e1d620682c2eb79ff2bb54d91697f76a7c
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-18 04:10:22 +0000

    created script for exercise 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * is_palindrome - function to know if the list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if its not a palindrome or 1 f it is
+ */
+int is_palindrome(listint_t **head)
+{
+	return (0);
+}
