b2498fdfdcb2e25eb3eb29aba54284e84552239d
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-18 03:58:43 +0000

    fixed with correct index

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,13 +2,10 @@
 def divisible_by_2(my_list=[]):
         a = 0
         t = []
-        if my_list:
-                while a < len(my_list):
-                        if a % 2 == 0:
-                                t.append(True)
-                        else:
-                                t.append(False)
-                        a = a + 1
-                return t
-        else:
-                return False
+        while a < len(my_list):
+                if my_list[a] % 2 == 0:
+                        t.append(True)
+                else:
+                        t.append(False)
+                a = a + 1
+        return t
