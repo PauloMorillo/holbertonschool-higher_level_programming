7426053c6fc4517723fbb6cabcd38812bc885c9d
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-19 18:25:10 +0000

    fixed remove condition

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,13 +4,12 @@ def square_matrix_simple(matrix=[]):
     b = 0
     c = []
     d = []
-    if matrix:
-        while a < len(matrix[0]):
-            while b < len(matrix):
-                c = c + [matrix[a][b] ** 2]
-                b = b + 1
-            b = 0
-            d = d + [c]
-            c = []
-            a = a + 1
-        return d
+    while a < len(matrix[0]):
+        while b < len(matrix):
+            c = c + [matrix[a][b] ** 2]
+            b = b + 1
+        b = 0
+        d = d + [c]
+        c = []
+        a = a + 1
+    return d
