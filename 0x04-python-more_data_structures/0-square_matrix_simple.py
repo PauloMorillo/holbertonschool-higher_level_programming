acd714e601324b55d6b8b6709c79276b9288ec00
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-19 18:30:15 +0000

    fixed with right

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,8 @@ def square_matrix_simple(matrix=[]):
     b = 0
     c = []
     d = []
-    while a < len(matrix[0]):
-        while b < len(matrix):
+    while a < len(matrix):
+        while b < len(matrix[0]):
             c = c + [matrix[a][b] ** 2]
             b = b + 1
         b = 0
