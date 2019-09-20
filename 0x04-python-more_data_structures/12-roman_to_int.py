a48d786b3896969c29db702867181c1dff0d8129
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-20 01:33:22 +0000

    fixed conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,14 +5,14 @@ def roman_to_int(roman_string):
             nrom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
                     "M": 1000}
             a = 1
-            if nrom[roman_string[a - 1]]:
+            if roman_string[a - 1] in nrom:
                 intro = [nrom[roman_string[a - 1]]]
             else:
                 return 0
             maxi = 0
             while a < len(roman_string):
                 b = roman_string[a]
-                if nrom[b]:
+                if b in nrom:
                     intro = intro + [nrom[b]]
                 else:
                     return 0
