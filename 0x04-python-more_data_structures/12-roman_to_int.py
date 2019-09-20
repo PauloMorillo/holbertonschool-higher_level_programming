b1529a83883d281af5e1d7070bf1bb7b0b1ed72e
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-20 01:10:01 +0000

    created script for exercise 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        if roman_string.isalpha():
+            nrom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
+                    "M": 1000}
+            a = 1
+            intro = [nrom[roman_string[a - 1]]]
+            maxi = 0
+            while a < len(roman_string):
+                b = roman_string[a]
+                intro = intro + [nrom[b]]
+                if nrom[b] > nrom[roman_string[a - 1]]:
+                    maxi = 1
+                a = a + 1
+            if maxi == 1:
+                return sum(intro) - 2
+            else:
+                return sum(intro)
+        else:
+            return 0
+    else:
+        return 0
