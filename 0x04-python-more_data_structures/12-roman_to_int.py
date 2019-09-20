9a2d55d0f61c9a444a38ac99fb4ffc1c88af87a4
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-20 03:39:35 +0000

    add a lot of conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,23 +2,40 @@
 def roman_to_int(roman_string):
     if roman_string:
         if roman_string.isalpha():
+            roman = roman_string
+            if "VV" in roman or "LL" in roman or "DD" in roman:
+                return 0
+            if (
+                    "IL" in roman or "IC" in roman or "ID" in roman or "IM"
+                    in roman):
+                return 0
+            if "XD" in roman or "XM" in roman:
+                return 0
+            if (
+                    "IIII" in roman or "XXXX" in roman or "CCCC" in roman or
+                    "MMMM" in roman):
+                return 0
             nrom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
                     "M": 1000}
             a = 1
-            if roman_string[a - 1] in nrom:
-                intro = [nrom[roman_string[a - 1]]]
+            rep = 0
+            if roman[a - 1] in nrom:
+                intro = [nrom[roman[a - 1]]]
             else:
                 return 0
             maxi = 0
-            while a < len(roman_string):
-                b = roman_string[a]
+            while a < len(roman):
+                b = roman[a]
                 if b in nrom:
                     intro = intro + [nrom[b]]
                 else:
                     return 0
-                if nrom[b] > nrom[roman_string[a - 1]]:
+                if nrom[b] > nrom[roman[a - 1]]:
                     maxi = maxi + 1
-                    subst = nrom[roman_string[a - 1]]
+                    subst = nrom[roman[a - 1]]
+                    rep = 0
+                if nrom[b] <= nrom[roman[a - 1]] and maxi == 1:
+                    return 0
                 a = a + 1
             if maxi == 1:
                 return sum(intro) - (subst * 2)
