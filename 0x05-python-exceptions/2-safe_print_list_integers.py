fa731004e0c79bbfcf201db7f5ad063e495e7056
PauloMorillo <pauloan@hotmail.com>
2019-09-30 23:16:47 +0000

    fixed with real number print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     a = 0
+    b = 0
     while a < x:
         try:
             print("{:d}".format(my_list[a]), end="")
             a = a + 1
+            b = b + 1
         except TypeError:
             a = a + 1
         except ValueError:
             a = a + 1
     print("")
-    return a
+    return b
