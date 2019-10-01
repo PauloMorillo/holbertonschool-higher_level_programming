4911931b4287296b38f567454b54f521475fc0bf
PauloMorillo <pauloan@hotmail.com>
2019-10-01 01:40:42 +0000

    created script for exercise 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,21 +5,19 @@ def list_division(my_list_1, my_list_2, list_length):
     while a < list_length:
         try:
             divi = my_list_1[a] / my_list_2[a]
-            an = an + [divi]
             a = a + 1
         except TypeError:
-            divi = [0]
-            an = an + divi
+            divi = 0
             a = a + 1
             print("wrong type")
         except ZeroDivisionError:
-            a = a + 1
-            divi = [0]
-            an = an + divi
+            divi = 0
             print("division by 0")
+            a = a + 1
         except:
-            divi = [0]
-            an = an + divi
+            divi = 0
             a = a + 1
             print("out of range")
+        finally:
+            an = an + [divi]
     return an
