732d150e0092ff71e7bedf4d28cb4fd6627d79e0
PauloMorillo <pauloan@hotmail.com>
2019-10-02 01:39:49 +0000

    fixed message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,10 +5,8 @@ docstring, or as a docstring on the __init__ method itself."""
 
 class Square:
     def __init__(self, size=0):
-        try:
-            if size > 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        except TypeError:
+        self.__size = size
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
