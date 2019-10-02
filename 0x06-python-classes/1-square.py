e6edcbf9461bd454ae2a19241c87dcbe30a6f849
PauloMorillo <pauloan@hotmail.com>
2019-10-02 01:20:15 +0000

    fixed with correct address

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+"""The __init__ method may be documented in either the class level docstring,
+or as a docstring on the __init__ method itself."""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
