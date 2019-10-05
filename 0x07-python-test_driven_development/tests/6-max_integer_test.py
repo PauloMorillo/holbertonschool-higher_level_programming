4ca9ea91c3dcdeb0f8f496980096dc8c9495b5af
PauloMorillo <pauloan@hotmail.com>
2019-10-05 23:04:31 +0000

    fixed indent

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,20 +10,20 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
-        def test_repeatmax(self):
-            self.assertEqual(max_integer([1, 2, 3, 4, 4]), 4)
+    def test_repeatmax(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 4]), 4)
 
-        def test_negativevalues(self):
-            self.assertEqual(max_integer([-5, -4, -5, -4, -1]), -1)
+    def test_negativevalues(self):
+        self.assertEqual(max_integer([-5, -4, -5, -4, -1]), -1)
 
-        def test_negativevaluesandzero(self):
-            self.assertEqual(max_integer([0, 0, 0, 0, -1]), 0)
+    def test_negativevaluesandzero(self):
+        self.assertEqual(max_integer([0, 0, 0, 0, -1]), 0)
 
-        def test_stringslist(self):
-            self.assertEqual(max_integer(["a", "b", "c"]), "c")
+    def test_stringslist(self):
+        self.assertEqual(max_integer(["a", "b", "c"]), "c")
 
-        def test_onlyonevalue(self):
-            self.assertEqual(max_integer([5]), 5)
+    def test_onlyonevalue(self):
+        self.assertEqual(max_integer([5]), 5)
 
 if __name__ == '__main__':
     unittest.main()
