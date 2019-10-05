f7f596334c592fccbabce893c63e70c45feff3f7
PauloMorillo <pauloan@hotmail.com>
2019-10-05 22:23:32 +0000

    fixed condition

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,22 +21,25 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if type(matrix) == list and type(matrix[0]) == list:
         if len(matrix[0]) < 1:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError('matrix must be a matrix (list of lists) of '
+                            'integers/floats')
         newma = [matrix[0][:] for i in matrix]
         lenr = len(matrix[0])
         a = 0
         while a < len(matrix):
             b = 0
             while b < len(matrix[a]):
-
                 if type(matrix[a][b]) != int and type(matrix[a][b]) != float:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError("matrix must be a matrix (list of lists) "
+                                    "of integers/floats")
                 newma[a][b] = round((matrix[a][b] / div), 2)
                 b = b + 1
-            if a < len(matrix[a]) - 1:
+            if a < len(matrix) - 1:
                 if lenr != len(matrix[a + 1]):
-                    raise TypeError("Each row of the matrix must have the same size")
+                    raise TypeError("Each row of the matrix must have the same"
+                                    " size")
             a = a + 1
         return newma
     else:
-         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
