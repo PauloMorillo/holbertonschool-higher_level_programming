428d015bdded4f99442cf47d04dcbde6e1e7e48a
PauloMorillo <pauloan@hotmail.com>
2019-10-06 02:45:40 +0000

    created script 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""My function to multiply a two matrix"""
+
+
+import numpy as np
+
+
+def matrix_mul(m_a, m_b):
+    """My multiplication function
+    Args:
+        m_a: first matrix
+        m_b: second matrix
+    Returns:
+        The return value. m_a * m_b
+    """
+    if type(m_a) != list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) != list:
+        raise TypeError("m_b must be a list")
+#    if len(m_a[0]) < 1:
+#        raise ValueError("m_a can’t be empty")
+#    if len(m_b[0]) < 1:
+#        raise ValueError("m_b can’t be empty")
+    a = np.array(m_a)
+    b = np.array(m_b)
+    return a.dot(b)
