ee8769d8935418a77d94ecc88e5551d005395cf4
PauloMorillo <pauloan@hotmail.com>
2019-10-16 04:49:50 +0000

    created script for exercise 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" module with function inherits_from"""
+
+
+class BaseGeometry:
+        """Class BaseGeometry"""
+        def area(self):
+                """Function to raise an error"""
+                raise Exception("area() is not implemented")
+
+        def integer_validator(self, name, value):
+                """Function to validates value"""
+                if type(value) != int:
+                        raise TypeError("{} must be an integer".format(name))
+                if value <= 0:
+                        raise ValueError("{} must be greater "
+                                         "than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+        """Class Rectangle that inherits from BaseGeometry"""
+        def __init__(self, width, height):
+                """Function init all begins here"""
+                super().integer_validator("width", width)
+                self.__width = width
+                super().integer_validator("height", height)
+                self.__height = height
+
+        def area(self):
+                """Function to know area of rectangle"""
+                return self.__width * self.__height
+
+        def __str__(self):
+                """Function to return a string"""
+                return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+        """Class Square inherit from rectangle"""
+        def __init__(self, size):
+                """Function to aign size"""
+                self.integer_validator("size", size)
+                self.__size = size
+                super().__init__(self.__size, self.__size)
+
+        def __str__(self):
+                """Function to return string"""
+                return "[Square] {}/{}".format(self.__size, self.__size)
