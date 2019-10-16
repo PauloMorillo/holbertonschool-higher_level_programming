27dca04124d431c2fc157a4ef942655d405bad0c
PauloMorillo <pauloan@hotmail.com>
2019-10-16 23:51:48 +0000

    fixed with while

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,9 +2,8 @@
 def number_of_lines(filename=""):
     """Function to return number of lines"""
     with open(filename, 'r') as f:
-        data = f.readline()
-        lines = 1
-        for line in f:
+        lines = 0
+        while f.readline():
             lines = lines + 1
         return lines
     f.close()
