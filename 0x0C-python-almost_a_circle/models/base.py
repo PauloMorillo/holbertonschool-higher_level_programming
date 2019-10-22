c4a29d0b9d695c0115c833123a600a650c49e55c
PauloMorillo <pauloan@hotmail.com>
2019-10-22 01:10:16 +0000

    update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module with base"""
 
+import json
+
 
 class Base:
     """Class base"""
@@ -14,3 +16,36 @@ class Base:
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Function from dictionari to json"""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Function to save list in a file"""
+        with open(cls.__name__ + ".json", 'w') as f:
+            if not list_objs:
+                f.write("[]")
+            else:
+                te = ""
+                i = 0
+                while i < len(list_objs):
+                    te = te + cls.to_json_string(list_objs[i].to_dictionary())
+                    i = i + 1
+                f.write(te)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Function to change from json to lsit"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return [json.loads(json_string)]
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Fucntion to returns an instance with all attributes already set"""
+        pass
