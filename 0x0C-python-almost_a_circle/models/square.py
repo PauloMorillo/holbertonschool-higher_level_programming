769c5eca73b61ee521fb16b88c3a7aed23ee802e
PauloMorillo <pauloan@hotmail.com>
2019-10-22 21:54:40 +0000

    remove and add widht and height at top

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,8 @@ class Square(Rectangle):
         """Square begins here"""
         super().__init__(size, size, x, y, id)
         self.size = size
+        self.width = size
+        self.height = size
 
     def __str__(self):
         """Function to Return string"""
@@ -29,8 +31,6 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        #self.width = value
-        #self.height = value
         self.__size = value
 
     def update(self, *args, **kwargs):
