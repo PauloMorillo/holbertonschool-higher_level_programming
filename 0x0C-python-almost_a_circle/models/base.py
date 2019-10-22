2d111c246e6be6e8d8e0520ce694e63d0e45ff2a
PauloMorillo <pauloan@hotmail.com>
2019-10-22 23:16:24 +0000

    remove condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,15 +28,13 @@ class Base:
     def save_to_file(cls, list_objs):
         """Function to save list in a file"""
         with open(cls.__name__ + ".json", 'w') as f:
-            if not list_objs:
-                f.write("[]")
-            else:
-                te = ""
+            t = []
+            if list_objs is not None:
                 i = 0
                 while i < len(list_objs):
-                    te = te + cls.to_json_string(list_objs[i].to_dictionary())
+                    t.append(list_objs[i].to_dictionary())
                     i = i + 1
-                f.write(te)
+            f.write(cls.to_json_string(t))
 
     @staticmethod
     def from_json_string(json_string):
@@ -48,4 +46,18 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Fucntion to returns an instance with all attributes already set"""
-        pass
+        du = cls(1, 1, 1, 1)
+        du.update(**dictionary)
+        return du
+
+    @classmethod
+    def load_from_file(cls):
+        """Function to load a list of instances"""
+        with open(cls.__name__ + ".json", 'r') as f:
+            if not f:
+                return []
+            lisob = cls.from_json_string(f.read())
+            lisnob = []
+            for dic in range(len(lisob)):
+                lisnob = lisnob + [cls.create(**lisob[0][dic])]
+            return lisnob
