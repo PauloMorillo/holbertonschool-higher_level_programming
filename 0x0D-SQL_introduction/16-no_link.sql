d57f5340bf650b122574fb87c861e54aeacd0423
PauloMorillo <pauloan@hotmail.com>
2019-11-25 23:18:13 +0000

    ordered to fix 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- this script list all records
 -- list all with exist name
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
