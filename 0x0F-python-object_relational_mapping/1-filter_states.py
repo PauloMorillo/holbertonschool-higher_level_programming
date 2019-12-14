fe753c2473e77f440814dff5d17fca83305a0fb1
PauloMorillo <pauloan@hotmail.com>
2019-12-14 21:59:39 +0000

    created script for exercise 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This Module prints all of a database"""
+import sys
+import MySQLdb
+
+
+def main():
+    """main function"""
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N'  ORDER BY id")
+    rows = cur.fetchall()
+    for _row in rows:
+        print(_row)
+if __name__ == '__main__':
+    main()
