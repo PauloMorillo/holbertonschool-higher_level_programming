e9c5bda730bfa9f3c89006546c62f76d506e0d5f
PauloMorillo <pauloan@hotmail.com>
2019-12-14 22:31:10 +0000

    fixed with binary to convert value

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ def main():
                          db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id"
+    cur.execute("SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id"
                 .format(sys.argv[4]))
     rows = cur.fetchall()
     for _row in rows:
