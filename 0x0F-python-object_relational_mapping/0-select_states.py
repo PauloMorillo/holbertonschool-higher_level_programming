7910f591baa523ac39ceb4ace70ddceeeb88d371
PauloMorillo <pauloan@hotmail.com>
2019-12-14 21:40:09 +0000

    fixed with regular use of mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,34 +2,14 @@
 """This Module prints all of a database"""
 import sys
 import MySQLdb
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Date
-from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """main function"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base = declarative_base()
-
-    class State(Base):
-        """Class state"""
-        __tablename__ = 'states'
-        id = Column(Integer)
-        name = Column(String(256), primary_key=True)
-
-        def __init__(self, name):
-            """All begins here"""
-            self.name = name
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("({}, '{}')".format(state.id, state.name))
-    session.close()
-if __name__ == "__main__":
-    main()
+db = MySQLdb.connect(host='localhost',
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3],
+                     port=3306)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id")
+rows = cur.fetchall()
+for _row in rows:
+    print(_row)
