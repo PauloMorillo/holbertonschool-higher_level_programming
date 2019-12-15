79192b1dcdd3c73b3308b1696891c33e5b6c275e
PauloMorillo <pauloan@hotmail.com>
2019-12-15 04:14:31 +0000

    fixed with filter

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,24 +7,19 @@ from sqlalchemy import Column, String, Integer, Date
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def main():
     """main function"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
-
     session = Session()
-    a = 0
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).filter(State.id == 1).all():
         if state:
-            if a == 0:
-                print("{}: {}".format(state.id, state.name))
-                break
+            print("{}: {}".format(state.id, state.name))
         else:
             print("Nothing")
-        a = a + 1
     session.close()
 if __name__ == '__main__':
     main()
