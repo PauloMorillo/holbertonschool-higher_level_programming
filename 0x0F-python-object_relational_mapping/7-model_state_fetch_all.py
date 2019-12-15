bf0c5871c57d45e55715617ea2412e8f0e099409
PauloMorillo <pauloan@hotmail.com>
2019-12-15 03:38:18 +0000

    created script for exercise 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This Module prints all of a database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """main function"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+
+    class State(Base):
+        """State Class"""
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True, nullable=False,
+                    autoincrement=True)
+        name = Column(String(128), nullable=False)
+
+    Base.metadata.create_all(engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
+if __name__ == '__main__':
+    main()
