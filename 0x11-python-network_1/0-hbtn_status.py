f15dbbc1f31b04b41e073ddfcf7b87e25f9cca05
PauloMorillo <pauloan@hotmail.com>
2020-01-13 17:23:51 +0000

    fixed spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module fetches a url"""
 
-
-import urllib.request
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    html = response.read()
-print("Body response:")
-print("\t - type:", type(html))
-print("\t - content:", html)
-print("\t - utf8 content:", html.decode("utf-8"))
+if __name__ == "__main__":
+    import urllib.request
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print("Body response:")
+    print("    - type:", type(html))
+    print("    - content:", html)
+    print("    - utf8 content:", html.decode("utf-8"))
