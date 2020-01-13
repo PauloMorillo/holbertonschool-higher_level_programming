5095bf2fdbb09a9f4107d814843f82a46791ab6c
PauloMorillo <pauloan@hotmail.com>
2020-01-13 17:28:49 +0000

    avoid executed when import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module fetches a url"""
 
-import sys
-import urllib.request
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
