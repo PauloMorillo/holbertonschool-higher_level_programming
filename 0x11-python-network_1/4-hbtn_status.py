66972bf3f5219adfcfef92d98f2437f631d2b7f2
PauloMorillo <pauloan@hotmail.com>
2020-01-13 19:08:29 +0000

    created script for exercise 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module fetches a url"""
+
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://intranet.hbtn.io/status').text
+    print("Body response:")
+    print("\t - type: {}".format(type(req)))
+    print("\t - content: {}".format(req))
