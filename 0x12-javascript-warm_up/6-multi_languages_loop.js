220fbc278c2583c5b7319dab54b502a5bfae00d6
PauloMorillo <pauloan@hotmail.com>
2020-02-04 17:49:48 +0000

    fixed with Capitalize javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let con = 0; con < myVar.length; con++) {
   console.log(myVar[con]);
 }
