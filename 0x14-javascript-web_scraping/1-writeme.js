7ca58a7da755a8bd99d856a4ffa73ffc688dabac
PauloMorillo <pauloan@hotmail.com>
2020-02-11 17:39:50 +0000

    remove conditions

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv[2] && process.argv[3]) {
-  fs.writeFile(process.argv[2], process.argv[3], err => {
-    if (err) {
-      console.error(err);
-    }
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.error(err);
+  }
   // file written successfully
-  });
-}
+});
