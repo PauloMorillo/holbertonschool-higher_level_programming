f741258afe1635098c83d208206cec2484a00b0a
PauloMorillo <pauloan@hotmail.com>
2020-02-11 17:42:04 +0000

    remove condition

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  const fs = require('fs');
+const fs = require('fs');
 
-  fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
-}
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
