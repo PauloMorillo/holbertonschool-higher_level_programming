6164421f4175a29e104a77193dd633031cfaa9f0
PauloMorillo <pauloan@hotmail.com>
2020-06-05 22:30:20 +0000

    [update] change logic

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,9 +6,25 @@
  */
 int check_cycle(listint_t *list)
 {
-	if (list != NULL)
+	listint_t **tempmem = NULL;
+	listint_t *temp = NULL, *temp2 = NULL;
+
+	temp = list;
+	tempmem = &list;
+
+	if (list == NULL)
+		return (0);
+	if (temp->next)
+		if (temp->next)
+			temp2 = temp->next->next;
+
+	while (temp2 != NULL)
 	{
-		return (1);
+		temp = temp->next;
+		if (temp == *(tempmem) || temp == temp2)
+			return (1);
+		/* temp = temp->next; */
+		temp2 = temp2->next;
 	}
 	return (0);
 }
