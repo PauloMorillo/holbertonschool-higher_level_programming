6ae0949e16fea98969a5e98ef0c0544ee8ad1905
PauloMorillo <pauloan@hotmail.com>
2020-06-05 23:33:55 +0000

    [update] remove condition head cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,24 +6,36 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t **tempmem = NULL;
+	/* listint_t **tempmem = NULL; */
 	listint_t *temp = NULL, *temp2 = NULL;
 
 	temp = list;
-	tempmem = &list;
+	/* tempmem = &list; */
 
 	if (list == NULL)
 		return (0);
-	if (temp->next)
-		temp2 = temp->next;
+
+	if (temp->next && temp->next->next)
+		temp2 = temp->next->next;
+	else
+		return (0);
 
 	while (temp2 != NULL)
 	{
-		temp = temp->next;
-		if (temp == *(tempmem) || temp == temp2)
+		/* if (temp == *(tempmem) ||  */
+		if (temp == temp2)
 			return (1);
 		/* temp = temp->next; */
-		temp2 = temp2->next;
+		if (temp2->next)
+		{
+			if (temp2->next->next)
+				temp2 = temp2->next->next;
+			else
+				return (0);
+		}
+		else
+			return (0);
+		temp = temp->next;
 	}
 	return (0);
 }
