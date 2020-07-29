382b09de18ffd7d64a7850c6f11a4df7907da64d
PauloMorillo <pauloan@hotmail.com>
2020-07-29 01:11:58 +0000

    [update] added logic from geek for geeks

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -17,57 +17,111 @@ if n < 4:
     print('N must be at least 4')
     exit(1)
 
-q = 0
-queens = []
+k = 1
 
-while q < n:
-    i = 0
-    while i < n:
-        j = 0
-        while j < n:
-            # print(queens)
-            if len(queens) is 0:
-                queens = [[i, j]]
-            else:
-                add_q = 0
-                for pos_q in queens:
-                    left = pos_q[0] + pos_q[1]
-                    eq_l = i + j
-                    right = pos_q[1] - pos_q[0] + i
-                    if i == pos_q[0] or j == pos_q[1]\
-                            or eq_l == left or right == j:
-                        # print("aquí")
-                        # print(i, j, pos_q[0], pos_q[1], n)
-                        pass
-                    else:
-                        add_q = add_q + 1
-                        # print(i, j, add_q, "veces que entra")
-                if add_q == len(queens):
-                    # print("esta entrando")
-                    queens.append([i, j])
-                else:
-                    if j == n - 1 and queens[-1][0] != i:
-                        i, j = queens.pop()
-                        if j == n - 1 and i is not 0:
-                            i, j = queens.pop()
-                    # if j == n - 1:
-                        # print("estoy entrando para pop")
-                        # i, j = queens.pop()
-                        # q = q - 1
-                    if len(queens) == n:
-                        if queens[0][0] == 0:
-                            print(queens)
-                        i = queens[0][0]
-                        j = queens[0][1]
-                        # print(i, j, "esto es lo que devolvemos")
-                        q = 0 - 1
-                        queens = []
-                        if j == n - 1:
-                            i = n
-                            q = n
-                            break
-            j = j + 1
-        # print(i, "esta es la i")
+def printSolution(board):  
+    #print(board)
+    queens = []
+    global k 
+    #print(k, "-\n") 
+    k = k + 1
+    for i in range(n):  
+        for j in range(n): 
+            # print(board[i][j], end = " ") 
+            if board[i][j] == 1:
+                queens.append([i, j])
+        #print("\n") 
+    #print("\n")  
+    print(queens)
+  
+''' A utility function to check if a queen can  
+be placed on board[row][col]. Note that this  
+function is called when "col" queens are  
+already placed in columns from 0 to col -1.  
+So we need to check only left side for  
+attacking queens '''
+def isSafe(board, row, col) : 
+      
+    # Check this row on left side  
+    for i in range(col):  
+        if (board[row][i]):  
+            return False
+  
+    # Check upper diagonal on left side  
+    i = row 
+    j = col 
+    while i >= 0 and j >= 0: 
+        if(board[i][j]): 
+            return False; 
+        i -= 1
+        j -= 1
+  
+    # Check lower diagonal on left side  
+    i = row 
+    j = col 
+    while j >= 0 and i < n: 
+        if(board[i][j]): 
+            return False
         i = i + 1
-    q = q + 1
-# print(queens)
+        j = j - 1
+  
+    return True
+  
+''' A recursive utility function to solve N  
+Queen problem '''
+def solveNQUtil(board, col) : 
+      
+    ''' base case: If all queens are placed  
+    then return true '''
+    if (col == n):  
+        printSolution(board)  
+        return True
+  
+    ''' Consider this column and try placing  
+    this queen in all rows one by one '''
+    res = False
+    for i in range(n): 
+      
+        ''' Check if queen can be placed on  
+        board[i][col] '''
+        if (isSafe(board, i, col)):  
+          
+            # Place this queen in board[i][col]  
+            board[i][col] = 1;  
+  
+            # Make result true if any placement  
+            # is possible  
+            res = solveNQUtil(board, col + 1) or res;  
+  
+            ''' If placing queen in board[i][col]  
+            doesn't lead to a solution, then  
+            remove queen from board[i][col] '''
+            board[i][col] = 0 # BACKTRACK  
+          
+    ''' If queen can not be place in any row in  
+        this column col then return false '''
+    return res 
+  
+''' This function solves the N Queen problem using  
+Backtracking. It mainly uses solveNQUtil() to  
+solve the problem. It returns false if queens  
+cannot be placed, otherwise return true and  
+prints placement of queens in the form of 1s.  
+Please note that there may be more than one  
+solutions, this function prints one of the  
+feasible solutions.'''
+
+def solveNQ() : 
+  
+    board = [[0 for j in range(n)]  
+                for i in range(n)] 
+  
+    if (solveNQUtil(board, 0) == False):
+        pass
+      
+        # print("Solution does not exist")  
+        return
+    return
+  
+# Driver Code  
+solveNQ()  
